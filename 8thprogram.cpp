ef68a49361267b7c0a4ea6373269f61319a6f79f
CodeRoar-92 <coderoar92@gmail.com>
2024-02-23 21:04:06 +0500

    Table of 2,3,4,5...10

---
+++ 8thprogram.cpp
@@ -0,0 +1,12 @@
+//print the table of2,3,4,5,6,7,8,9,10.
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    int a=2,b=3,c=4,d=5,e=6,f=7,g=8,h=9,k=10;
+
+    for(int i=1; i<=n;i++){
+cout<<" "<<a*i<<"  "<<b*i<<"  "<<c*i<<"  "<<d*i<<"  "<<e*i<<"  "<<f*i<<"  "<<g*i<<"  "<<h*i<<"  "<<k*i<<endl;
+    }
+    return 0;
+}
