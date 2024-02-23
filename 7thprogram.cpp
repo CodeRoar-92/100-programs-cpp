8d75f478bc9df22abe8ec35f8c1df74373eece33
CodeRoar-92 <coderoar92@gmail.com>
2024-02-23 18:54:24 +0500

    Print even and odd numbers

---
+++ 7thprogram.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=99;
+    for(int i =1; i<=n; i +=2){ //print odd numbers.
+        cout<<"i = "<<i<<endl;
+    }
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=100;
+    for(int i=2; i<=n; i+=2){
+        cout<<"i = "<<i<<endl; //print even numbers.
+    }
+    return 0;
+}
