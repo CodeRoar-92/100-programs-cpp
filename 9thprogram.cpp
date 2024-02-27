f7c10dafc2e30c34a38584ed2d176728dae7ee85
CodeRoar-92 <160207296+CodeRoar-92@users.noreply.github.com>
2024-02-27 18:15:27 +0500

    Swapping of two Numbers using third varible

---
+++ 9thprogram.cpp
@@ -0,0 +1,11 @@
+//swapping of two numbers;
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10,b=20,c;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"The swapping values are:"<<a<<" "<<b;
+    return 0;
+}
