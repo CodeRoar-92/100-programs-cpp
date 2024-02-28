a5014f66591d5fd17f9c53a96a259b5f58d01761
CodeRoar-92 <160207296+CodeRoar-92@users.noreply.github.com>
2024-02-28 08:28:24 +0500

    area of circle,triangle

---
+++ 12thprogram.cpp
@@ -0,0 +1,24 @@
+ /*
+                   Area of the circle is
+                   Area = pi*r*r 
+                   Circumference of the circle is
+                   C =2*pi*r 
+                   Area of triangle is
+                   1/2 base*height
+                   */
+#include<iostream>
+using namespace std;
+int main(){
+float pi = 3.1412;
+int r=5;
+float Area = pi*r*r;
+float Circumfernec=2*pi*r;
+float area_of_triangle = r*r/2;
+cout<<"Area of the circle is: "<<Area<<endl;
+cout<<"Circumfernc of the circle is: "<<Circumfernec<<endl;
+cout<<"Area of the Triangle is: "<<area_of_triangle<<endl;
+return 0;
+}
+
+
+
