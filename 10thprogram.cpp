87fefbbe223f6c0aa8952c67dfea5cdc33be08f9
CodeRoar-92 <160207296+CodeRoar-92@users.noreply.github.com>
2024-02-28 20:15:46 +0500

    Add more programs

--- 10thprogram.cpp
+++ 10thprogram.cpp
@@ -8,3 +8,22 @@ int main(){
     cout<<"The value of 'a' after swapping is "<<a<<" and the value of 'b' is "<<b;
     return 0;
 }
+
+
+//Another program.
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Enter the a"<<endl;
+cin>>a;
+cout<<"Enter the b"<<endl;
+cin>>b;
+cout<<"Before Swapping the value of a and b is "<<a<<" "<<b<<endl;
+int t;
+t=a;
+a=b;
+b=t;
+cout<<"After Swapping the value of a and b is "<<a<<" "<<b<<endl;
+return 0;
+}
